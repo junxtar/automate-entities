7d04dc3f090da48fb1926a50fd5c3e091af40d48
junxtar <aa1870088@naver.com>
2025-05-09 10:06:41 +0900

    init

---
+++ src/main/java/com/example/junxtar/javaparser/JavaparserApplication.java
@@ -0,0 +1,13 @@
+package com.example.junxtar.javaparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaparserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaparserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/junxtar/javaparser/JavaparserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junxtar.javaparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaparserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
